52fec1f6579864f11239eabbe671e382f49699ea
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-12-15 19:51:19 +0530

    navbar

---
+++ contact-manager/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import styles from "./Navbar.module.css"
+
+function Navbar() {
+  return (
+    <div className={styles.container}>
+        <h3 className={styles.logo}>Contact Manager</h3>
+        <div className={styles.links}>
+            <Link className='transition ease-in-out delay-75 rounded-md md:px-4 sm:px-2 py-2 hover:bg-[#222222] duration-700' href="/">Home</Link>
+            {localStorage.getItem("token") && <Link className='transition ease-in-out delay-75 rounded-md md:px-4 sm:px-2 py-2 hover:bg-[#222222] duration-700' href="/">Profile</Link>}
+            {!localStorage.getItem("token") && <Link className='transition ease-in-out delay-75 rounded-md md:px-4 sm:px-2 py-2 hover:bg-[#222222] duration-700' href="/login">SignUp/Login</Link>}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
