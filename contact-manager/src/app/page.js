f0016ab022e48bfa8b14a7dec741dda276ca11c0
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-12-17 16:10:16 +0530

    homepage

--- contact-manager/src/app/page.js
+++ contact-manager/src/app/page.js
@@ -1,9 +1,21 @@
-
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
-      Hello
+    <main className="home">
+      <div className="cont1">
+        <img className="mobile_img" src="backg2.png" alt="cover_image" />
+        <div className="text_container">
+          <h3 className="cover_heading">Welcome to</h3>
+          <h3 className="cover_heading">Your Personal Contact Hub! 🌐✨</h3>
+          <h3 className="cover_sub_heading">Stay Connected, Stay Organized.</h3>
+          <p className="cover_paragraph">Effortlessly manage and cherish your friendships and relationships with our intuitive Contact Manager. Store, access, and update contact details of friends and loved ones in one secure place. Never lose touch again - because every connection matters.</p>
+        </div>
+      </div>
+      <Link href="/login" className="button bg-blue-950 max-w-max items-center my-5 rounded-lg px-4 py-2 md:text-2xl sm:text-lg">Get started now</Link>
+      <div className="cont2">
+        <img className="comp_img" src="backg1.jpg" alt="cover_image" />
+      </div>
     </main>
   )
 }
--- contact-manager/src/components/navbar/Navbar.jsx
+++ contact-manager/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import styles from "./Navbar.module.css"
 function Navbar() {
   return (
     <div className={styles.container}>
-        <h3 className={styles.logo}>Contact Manager</h3>
+        <h3 className={styles.logo}>Strong Bond</h3>
         <div className={styles.links}>
             <Link className='transition ease-in-out delay-75 rounded-md md:px-4 sm:px-2 py-2 hover:bg-[#222222] duration-700' href="/">Home</Link>
             {localStorage.getItem("token") && <Link className='transition ease-in-out delay-75 rounded-md md:px-4 sm:px-2 py-2 hover:bg-[#222222] duration-700' href="/">Profile</Link>}
